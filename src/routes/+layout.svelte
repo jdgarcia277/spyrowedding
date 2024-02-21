54b9550a8ddc80849c64711c47688f20feb03692
jdgarcia277 <jdgarcia277@yahoo.com>
2024-02-21 16:37:06 -0600

    created initial layout for site

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { isSmallScreen } from '$lib/screenSizeStore';
+    import MenuIcon from '$lib/MenuIcon.svelte';
+</script>
+
+{#if $isSmallScreen}
+    <nav class="mobile_navbar">
+        <MenuIcon />
+    </nav>
+{:else}
+    <nav class="navbar"></nav>
+{/if}
+
+<slot />
+
+<style>
+    .mobile_navbar {
+        display: flex;
+        justify-content: end;
+    }
+    .navbar {
+        display: flex;
+    }
+</style>
