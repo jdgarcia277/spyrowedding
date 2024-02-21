095d4b6269a4519da9a7bec65b2f62f2c9b65999
jdgarcia277 <jdgarcia277@yahoo.com>
2024-02-21 15:37:12 -0600

    created initial home page responsiveness

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang='ts'>
+    import { onMount } from "svelte";
+
+    let isSmallScreen: boolean = false;
+
+    function checkScreenSize() {
+        if (typeof window !== "undefined") {
+            isSmallScreen = window.innerWidth < 768;
+        }
+    }
+
+    onMount(() => {
+        checkScreenSize();
+        window.addEventListener('resize', checkScreenSize);
+        return () => {
+            window.removeEventListener('resize', checkScreenSize);
+        };
+    });
+
+</script>
+
+<div class={isSmallScreen ? 'home_container_small' : 'home_container_large'}>
+    {#if isSmallScreen}
+    <nav class="mobile_navbar"></nav>
+    {:else}
+    <nav class="navbar"></nav>
+    {/if}
+</div>
