fbbf8fa13a5c3ac28eb4ee2d29af15ff7b8a904c
jdgarcia277 <jdgarcia277@yahoo.com>
2024-02-21 18:30:56 -0600

    refactored layout for global css and not to hold navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,5 @@
 <script lang="ts">
-    import { isSmallScreen } from '$lib/screenSizeStore';
-    import MenuIcon from '$lib/MenuIcon.svelte';
+    import '../styles/global.css';
 </script>
 
-{#if $isSmallScreen}
-    <nav class="mobile_navbar">
-        <MenuIcon />
-    </nav>
-{:else}
-    <nav class="navbar"></nav>
-{/if}
-
 <slot />
-
-<style>
-    .mobile_navbar {
-        display: flex;
-        justify-content: end;
-    }
-    .navbar {
-        display: flex;
-    }
-</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,31 @@
 <script lang='ts'>
     import { isSmallScreen } from '$lib/screenSizeStore';
+    import MenuIcon from '$lib/MenuIcon.svelte';
 
 </script>
 
+{#if $isSmallScreen}
+    <div class="mobile_home_container">
+        <nav class="mobile_navbar">
+            <MenuIcon />
+        </nav>
+    </div>
+    
+{:else}
+    <div class="home_container">
+        <nav class="navbar"></nav>
+    </div>
+    
+{/if}
+
+<style>
+    .mobile_home_container {
+        height: 100vh;
+        width: 100vw;
+    }
+    .mobile_navbar {
+        display: flex;
+        justify-content: end;
+        gap: 10px;
+    }
+</style>
